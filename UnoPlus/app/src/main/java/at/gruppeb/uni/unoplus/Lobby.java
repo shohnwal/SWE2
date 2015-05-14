c3d196b96bd0204b82cffd929e1db3fa36a2d02c
benitsch <BernhardNitsch@gmx.at>
2015-05-14 11:06:20 +0200

    lobby Act
    hide notification bar und impl some clickmethods(TODO: ListView Popup to
    join a game or create one

--- UnoPlus/app/src/main/java/at/gruppeb/uni/unoplus/Lobby.java
+++ UnoPlus/app/src/main/java/at/gruppeb/uni/unoplus/Lobby.java
@@ -3,19 +3,63 @@ package at.gruppeb.uni.unoplus;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 /**
  * Created by Bernhard on 13.05.2015.
  */
 public class Lobby extends ActionBarActivity {
 
+    private Button btnSpielErstellen;
+    private Button btnSpielBeitreten;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getSupportActionBar().hide();
+        //Error:
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Remove the notification Bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_lobby);
 
+
+    }
+
+    public void init() {
+        setupBtnSpielErstellen();
+        setupBtnSpielBeitreten();
+    }
+
+    public void setupBtnSpielErstellen() {
+        btnSpielErstellen = (Button) findViewById(R.id.button_start);
+        btnSpielErstellen.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("Spiel Erstellen");
+                //TODO ListView Popup
+            }
+        });
+
+
+    }
+
+    public void setupBtnSpielBeitreten() {
+        btnSpielBeitreten = (Button) findViewById(R.id.button_join);
+        btnSpielBeitreten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("Spiel Beitreten");
+            }
+        });
 
     }
 
