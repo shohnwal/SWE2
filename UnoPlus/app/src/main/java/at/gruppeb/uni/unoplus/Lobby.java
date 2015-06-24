f2deb11bd4b7300d9a289176084f735438bd19ce
benitsch <BernhardNitsch@gmx.at>
2015-06-24 13:05:26 +0200

    dein name dein name
    .

--- UnoPlus/app/src/main/java/at/gruppeb/uni/unoplus/Lobby.java
+++ UnoPlus/app/src/main/java/at/gruppeb/uni/unoplus/Lobby.java
@@ -252,7 +252,7 @@ public class Lobby extends ActionBarActivity {
 
                 Intent nextScreen = new Intent("at.gruppeb.uni.unoplus.HostGame");
                 //Sending the Host- Player- name to the new Activity
-                nextScreen.putExtra("hostName", ("Dein Name" + mBltService.getPlayerName()));
+                nextScreen.putExtra("hostName", (mBltService.getPlayerName()));
                 startActivity(nextScreen);
 
 
